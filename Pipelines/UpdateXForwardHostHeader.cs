3eb44962601100f6cf1784d797cf92d94eabeaea
Owen chen <owen.chen@blackmores.com.au>
2021-06-23 08:55:20 +0800

    initial

---
+++ Pipelines/UpdateXForwardHostHeader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Sitecore.Configuration;
+using Sitecore.Pipelines.PreprocessRequest;
+
+namespace SitecoreContainerHelper.Pipelines
+{
+    public class UpdateXForwardHostHeader : PreprocessRequestProcessor
+    {
+        public override void Process(PreprocessRequestArgs args)
+        {
+            var context = HttpContext.Current;
+            string str = context?.Request?.Headers?[Settings.LoadBalancingHost];
+            if (!string.IsNullOrEmpty(str) && str.Contains(":"))
+            {
+                str = str.Substring(0, str.IndexOf(":", StringComparison.OrdinalIgnoreCase));
+                context.Request.Headers[Settings.LoadBalancingHost] = str;
+            }
+        }
+    }
+}
